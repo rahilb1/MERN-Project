2ed7b7540d16dc980fd690dfb6566f6d2906654e
Rahil Budhwani <budhwanirahil@gmail.com>
2023-07-27 19:20:02 +0530

    Footer created

--- course-bundler/src/App.js
+++ course-bundler/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Courses from './components/Courses/Courses';
 import Header from './components/Layout/Header/Header';
+import Footer from './components/Layout/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/courses" element={<Courses/>} />
       </Routes>
+      <Footer/>
     </Router>
   );
 }
