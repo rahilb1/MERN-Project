eb0c25eae9f4e1453fc21bce1cdfe0d5f06e0cbb
rahilb1 <budhwanirahil@gmail.com>
2023-10-22 13:35:25 +0530

    Created Custom Error Handler

--- course-bundler-server/app.js
+++ course-bundler-server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
+import ErrorMiddleware from "./middlewares/Error.js";
 
 config({
   path: "./config/config.env",
@@ -14,3 +15,5 @@ app.use("/api/v1/", course);
 app.use("/api/v1/", user);
 
 export default app;
+
+app.use(ErrorMiddleware)
--- course-bundler-server/controllers/courseController.js
+++ course-bundler-server/controllers/courseController.js
@@ -1,8 +1,9 @@
-import {Course} from '../models/Course.js'
-export const getAllCourses = async (req, res) => {
-    const courses = await Course.find();
-    res.status(200).json({
-        success: true,
-        courses,
-    });
-}
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import { Course } from "../models/Course.js";
+export const getAllCourses = catchAsyncError(async (req, res, next) => {
+  const courses = await Course.find();
+  res.status(200).json({
+    success: true,
+    courses,
+  });
+});
