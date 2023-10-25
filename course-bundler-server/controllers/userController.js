4feb82aa46d59bcf2bf786654a165994b7eb9833
rahilb1 <budhwanirahil@gmail.com>
2023-10-25 16:24:46 +0530

    Created Sign Up API

---
+++ course-bundler-server/controllers/userController.js
@@ -0,0 +1,28 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import { User } from "../models/User.js";
+import { sendToken } from "../utils/sendToken.js";
+
+export const register = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  //   const file = req.file;
+
+  if (!name || !email || !password )
+    return next(new ErrorHandler("Please enter all field", 400));
+
+  let user = await User.findOne({ email });
+
+  if (user) return next(new ErrorHandler("User Already Exist", 409));
+
+  user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "tempid",
+      url: "tempurl",
+    },
+  });
+
+  sendToken(res, user, "Registered Successfully", 201);
+});
--- course-bundler-server/routes/userRoutes.js
+++ course-bundler-server/routes/userRoutes.js
@@ -1,6 +1,23 @@
-import express from 'express';
+import express from "express";
+import { register } from "../controllers/userController.js";
 
 const router = express.Router();
 
+// To register a new user
+router.route("/register").post(register);
+
+// Login
+// Logout
+// Get my profile
+
+// ChangePassword
+// UpdateProfile
+// UpdateProfilePicture
+
+// ForgotPassword
+// ResetPassword
+
+// AddtoPlaylist
+// RemoveFromPlaylist
 
 export default router;
